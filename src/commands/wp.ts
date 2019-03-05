9f59690c5d4fd0de7fe153ccc5bdca24ba8c4573
gustavderdrache <aford@forumone.com>
2019-03-05 09:58:11 -0500

    Add `f1 wp` command

---
+++ src/commands/wp.ts
@@ -0,0 +1,35 @@
+import { Command, flags } from '@oclif/command';
+
+import runComposeAsUser from '../docker/runComposeAsUser';
+import findProject from '../project/findProject';
+
+export default class Wp extends Command {
+  static description = 'run wp-cli commands';
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    'dry-run': flags.boolean({
+      description: 'print command instead of running it',
+    }),
+  };
+
+  static strict = false;
+
+  async run() {
+    const { argv, flags } = this.parse(Wp);
+
+    const project = await findProject();
+    if (project === null || project.type !== 'compose') {
+      return this.error(
+        'Could not find a docker-compose.yml file in the current directory or any parent',
+        { exit: 1 },
+      );
+    }
+
+    return runComposeAsUser('wp', argv, {
+      cwd: project.root,
+      dryRun: flags['dry-run'],
+      extraFiles: ['docker-compose.cli.yml'],
+    });
+  }
+}
