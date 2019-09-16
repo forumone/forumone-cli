f8d63ca10ed88b0d1fffa227a9e7a80229253303
Kelli Monahan <kmonahan@forumone.com>
2019-09-16 14:17:17 -0500

    Further trimming the rule list

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
         "prettier/@typescript-eslint"
     ],
     "rules": {
-        "@typescript-eslint/await-thenable": "off",
         "@typescript-eslint/explicit-member-accessibility": [
             "off",
             {
@@ -27,16 +26,6 @@ module.exports = {
             }
         ],
         "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/member-naming": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-param-reassign": "off",
-        "@typescript-eslint/no-parameter-properties": "off",
-        "@typescript-eslint/no-require-imports": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-use-before-declare": "off",
-        "@typescript-eslint/promise-function-async": "off",
-        "@typescript-eslint/require-await": "off",
-        "@typescript-eslint/restrict-plus-operands": "off",
         "@typescript-eslint/triple-slash-reference": ["error", {
           "path": "never",
           "types": "never",
@@ -47,23 +36,12 @@ module.exports = {
             "off",
             "as-needed"
         ],
-        "capitalized-comments": "off",
-        "complexity": "off",
-        "default-case": "off",
         "implicit-dependencies/no-implicit": ["error", { optional: true }],
-        "max-classes-per-file": "off",
         "no-bitwise": "error",
-        "no-cond-assign": "off",
         "no-console": "error",
-        "no-empty-functions": "off",
-        "no-invalid-this": "off",
-        "no-irregular-whitespace": "off",
         "no-restricted-syntax": "off",
         "object-shorthand": "error",
         "oclif/command-class-variables": "off",
-        "one-var": "error",
-        "prefer-const": "error",
-        "prefer-template": "off",
-        "valid-typeof": "off"
+        "prefer-const": "error"
     }
 };
